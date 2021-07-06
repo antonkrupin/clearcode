f24cb984389a20166c9daf8b0e11e4cc969f01d3
antonkrupin <krupin_anton@mail.ru>
2021-07-06 09:24:36 +0300

    Update variables_names_4.py

--- variables_names_4.py
+++ variables_names_4.py
@@ -1,1 +1,12 @@
-
+1.
+def MaximumDiscount(quantityOfGoods, prices):
+    if(quantityOfGoods < 3):
+        return 0
+    if(quantityOfGoods == 3):
+        return min(prices)
+      
+def MaximumDiscount(numOfGoods, prices):
+    if(numOfGoods < 3):
+        return 0
+    if(numOfGoods == 3):
+        return min(prices)
