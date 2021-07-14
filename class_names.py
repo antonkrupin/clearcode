4d33e328e4c04c838a14de211934ff21f4622ca4
antonkrupin <krupin_anton@mail.ru>
2021-07-14 08:49:31 +0300

    Update class_names.py

--- class_names.py
+++ class_names.py
@@ -12,4 +12,20 @@ class Rectangle:
     def rectangle_area(self):
         return self.width * self.height
       
-    def 
+class Triangle:
+  def __init__(self, base_line, height):
+        self.base_line = base_line
+        self.height = height
+        
+    def triangle_area(self):
+        return 0,5 * self.base_line * self.height
+      
+//для названия классов использовал существительное
+//которое достаточно понятно описывает, что
+//содержится в классе
+//в обоих классах есть методы, которые похожи друг на друга
+//rectangle_area и triangle_area - возвращают площадь фигуры
+//чтобы не запутаться и не использовать какие-то синонимы
+//для разных методов
+//в качестве идентификатора в начале каждого метода
+//используется наименование класса, для которого он используется
