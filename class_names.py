0ab54d1eaf13a67bd00325b27ab690f68fadc74a
antonkrupin <krupin_anton@mail.ru>
2021-07-14 08:44:11 +0300

    Update class_names.py

--- class_names.py
+++ class_names.py
@@ -1,1 +1,15 @@
+//к сожалению в своем коде я не пользовался классами в той мере
+//в которой смогу применить занния из данного занятия
+//сам код проекта был не очень большим
+//и использование классов ограничивалось одним классом
 
+
+class Rectangle:
+  def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+    def rectangle_area(self):
+        return self.width * self.height
+      
+    def 
