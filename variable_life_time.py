63cf2dce4c47de289e7b5ee2e6298761c3527a26
antonkrupin <krupin_anton@mail.ru>
2021-08-23 05:47:34 +0300

    Update variable_life_time.py

--- variable_life_time.py
+++ variable_life_time.py
@@ -1,1 +1,8 @@
-
+1. Выделяем в отдельный метод  
+ for (let i = index; i < count - 1; i++) {
+    array[i] = array[i + 1];
+};
+2. Выделяем в отдельный метод
+for (let i = count; i > index; i--) {
+    array[i] = array[i - 1]
+};
