b5ee43c5b3f71aa655c04d7de3a22ce967862556
antonkrupin <krupin_anton@mail.ru>
2021-09-05 09:43:16 +0300

    Update array.py

--- array.py
+++ array.py
@@ -1,1 +1,19 @@
+//код на js
+const generateExpression = () => {
+  const expressionsSignArray = ['+', '-', '*'];
+  const expressionNumber = getRandom(expressionsSignArray.length);
+  const expression = {
+    firstNumber: getRandom(100),
+    secondNumber: getRandom(100),
+    sign: expressionsSignArray[expressionNumber],
+    expression() {
+      return `${expression.firstNumber} ${expression.sign} ${expression.secondNumber}`;
+    },
+  };
+  return expression;
+};
 
+//изначально expression была представленна массивом,
+//который возвращался функцией
+//но при обращении к элементам массива
+//легко было ошибиться, с объектом это все более наглядно
